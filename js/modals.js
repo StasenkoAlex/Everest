1de262531a0883647d276845e7f69ad1cf475ef4
Nick Borisov <n.borisov.ua@gmail.com>
2017-11-29 13:58:33 +0200

    Добавлен шаблон вывода и обработчик для упрощенной модалки коттеджей

--- js/modals.js
+++ js/modals.js
@@ -1,39 +1,63 @@
 $(document).ready(function(){
 
-  var modal = $('.js-modal-simple'),
-      modalName = $('.js-modal-name'),
-      modalDescription = $('.js-modal-desciption');
-
-  function addText(name, description) {
-    modalName.text(name),
-    modalDescription.html(description);
+  var $document = $(document),
+      $body = $('body'),
+      modal = $('.js-modal'),
+      modalWindow;
+  
+  //Функция с набором инструкций для закрытия модального окна
+  function closeModal(){
+    modalWindow.removeClass('is-active');
+    modal.removeClass('is-opened');
+    $body.removeClass('is-scroll');
   }
-
-  $(document).on('click','.js-modal-link-simple', function(e) {
-    $('body').addClass('is-scroll');
-    e.preventDefault();
-    $('.js-modal-simple').addClass('is-active');
-
-    //опереедляем индекс населенного пунтка в массиве
-    var modalIndex = $(this).data('modal');
-      console.log(townships[modalIndex].name);
-      addText(townships[modalIndex].name,townships[modalIndex].desc);
-   
-
+  
+  $document.on('click', '.js-modal-link', function(){
+    var self = $(this),
+        modalContent = self.data('content'),
+        modalGallery;
+    //запоминаем активную модалку
+    modalWindow = $('#'+self.data('window'));
+    //открываем модалку
+    modal.addClass('is-opened');
+    modalWindow.addClass('is-active');
+    $body.addClass('is-scroll');
+    //подставляем контент из массива, если есть
+    if (typeof(modalContent) !== 'undefined') {
+      for(var i=0; i<projectsData.length; i++) {
+        if ( projectsData[i].name == modalContent) {
+          modalContent = projectsData[i].content;
+          modalWindow.html(modalContent);
+        }
+      }
+    } else {
+      return;
+    }
+    //формируем из изображений слайдер
+    modalGallery = modalWindow.find('.js-gallery');
+    modalGallery.slick({
+      slidesToShow: 2,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 1,
+            dots: true,
+            arrows: false
+          }
+        }
+      ]
+    });
   });
-
-  $('body').click(function(e) {
-    if($('.js-modal-simple').has(e.target).length == 0 && $('.js-modal-link-simple').has(e.target).length == 0) {
-      $('.js-modal-simple').removeClass('is-active');
-      $('body').removeClass('is-scroll');
+  
+  //Закрываем модалку по клику вне области
+  $body.on('click', function(e){
+    var $target = $(e.target);
+    if (modalWindow && $target.closest(modalWindow).length == 0) {
+      closeModal();
     }
   });
+  //Закрываем модалку по клику на крестик
+  $document.on('click', '.js-modal-close', closeModal);
 
 });
-
-
-  
-
-
-
-
